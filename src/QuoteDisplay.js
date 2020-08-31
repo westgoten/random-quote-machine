19e50674421f70351dc186a59b7d24ce473206d8
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-08-31 12:22:02 -0300

    Make some visual changes to QuoteDisplay component

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -7,14 +7,16 @@ class QuoteDisplay extends React.Component {
             <div id='quote-box'>
                 <div id='text-box'>
                     <i className='fas fa-quote-left'></i>
-                    <span id='text'>Some text</span>
+                    <span id='text'>Some text aaaa aaa aaa aaaa aaa aaaa aaaaaaaaaa</span>
                 </div>
                 <p id='author'>- Author</p>
                 <div id='buttons'>
                     <a id='tweet-quote' href='https://twitter.com/intent/tweet' target='_blank' rel="noopener noreferrer">
                         <i className='fab fa-twitter'></i>
                     </a>
-                    <a id='tumblr-quote'><i className='fab fa-tumblr'></i></a>
+                    <a id='tumblr-quote'>
+                        <i className='fab fa-tumblr'></i>
+                    </a>
                     <button id='new-quote'>New quote</button>
                 </div>
             </div>

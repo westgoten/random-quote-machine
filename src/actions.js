250579e33653958f6c6507a7bf36e973ab059b27
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-09-03 12:30:42 -0300

    Implement action creators and redux store's initial state

--- src/actions.js
+++ src/actions.js
@@ -1,2 +1,29 @@
-const START_REQUEST = 'START_REQUEST'
-const REQUEST_RESPONSE = 'REQUEST_RESPONSE'
+const START_REQUEST = 'start_request'
+const REQUEST_RESPONSE = 'request_response'
+
+export function startRequest() {
+    return {
+        type: START_REQUEST
+    }
+}
+
+export function requestResponse(payload) {
+    return {
+        type: REQUEST_RESPONSE,
+        payload
+    }
+}
+
+export function getQuotes(url) {
+    return (dispatch) => {
+        dispatch(startRequest())
+
+        fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62\
+        d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+        .then((response) => response.json)
+        .then(data => {
+            dispatch(requestResponse(data))
+        })
+        .catch(error => console.error(error))
+    }
+}
--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,12 @@
 import { createReducer } from '@reduxjs/toolkit'
 import { START_REQUEST, REQUEST_RESPONSE } from './actions'
 
-const initialState = {}
+const initialState = {
+    quoteList: [],
+    currentQuote: {},
+    hasRequestedData: false,
+    isFetchingDone: false
+}
 
 const quotesReducer = createReducer(initialState, {
     START_REQUEST: (state, action) => {},

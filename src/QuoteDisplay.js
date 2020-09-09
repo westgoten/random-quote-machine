a6c7de8ee75658a30ac17e7fa6870dcbc17375c8
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-09-09 12:22:05 -0300

    [WIP] Apply animations to QuoteDisplay component's buttons and #wrapper background-color when the quote is updated

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -1,29 +1,58 @@
 import React from 'react'
 import { fetchQuotes, changeQuote } from './actions'
 import { connect } from 'react-redux'
+import { getRandomInt } from './utils'
 import './QuoteDisplay.css'
 
 class QuoteDisplay extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            color: 'black',
+            opacity: 1
+        }
+        this.colors = ['red', 'green', 'blue']
+        this.updateQuote = this.updateQuote.bind(this)
+    }
+
     componentDidMount() {
-        this.props.fetchQuotes()
+        this.props.fetchQuotes().then(() => {
+            this.setState({ color: this.getRandomColor() })
+        })
+    }
+
+    updateQuote() {
+        this.props.changeQuote()
+        this.setState({ color: this.getRandomColor() })
+    }
+
+    getRandomColor() {
+        return this.colors[getRandomInt(this.colors.length)]
     }
 
     render() {
         return (
-            <div id='quote-box'>
-                <div id='text-box'>
-                    <i className='fas fa-quote-left'></i>
-                    <span id='text'>{this.props.currentQuote.quote}</span>
-                </div>
-                <p id='author'>- {this.props.currentQuote.author}</p>
-                <div id='buttons'>
-                    <a id='tweet-quote' href='https://twitter.com/intent/tweet' target='_blank' rel="noopener noreferrer">
-                        <i className='fab fa-twitter'></i>
-                    </a>
-                    <a id='tumblr-quote' href='https://www.tumblr.com/widgets/share/tool' target='_blank' rel="noopener noreferrer">
-                        <i className='fab fa-tumblr'></i>
-                    </a>
-                    <button id='new-quote' onClick={this.props.changeQuote}>New quote</button>
+            <div id='wrapper' style={{ color: this.state.color, backgroundColor: this.state.color }}>
+                <div id='quote-box'>
+                    <div id='text-box' style={{ opacity: this.state.opacity }}>
+                        <i className='fas fa-quote-left'></i>
+                        <span id='text'>{this.props.currentQuote.quote}</span>
+                    </div>
+                    <p id='author' style={{ opacity: this.state.opacity }}>
+                        - {this.props.currentQuote.author}
+                    </p>
+                    <div id='buttons'>
+                        <a id='tweet-quote' href='https://twitter.com/intent/tweet' target='_blank' 
+                        rel="noopener noreferrer" style={{ backgroundColor: this.state.color }}>
+                            <i className='fab fa-twitter'></i>
+                        </a>
+                        <a id='tumblr-quote' href='https://www.tumblr.com/widgets/share/tool' target='_blank' 
+                        rel="noopener noreferrer" style={{ backgroundColor: this.state.color }}>
+                            <i className='fab fa-tumblr'></i>
+                        </a>
+                        <button id='new-quote' onClick={this.updateQuote} 
+                        style={{ backgroundColor: this.state.color }}>New quote</button>
+                    </div>
                 </div>
             </div>
         )
@@ -33,7 +62,7 @@ class QuoteDisplay extends React.Component {
 function mapStateToProps(state) {
     return Object.assign({}, state)
 }
-  
+
 function mapDispatchToProps(dispatch) {
     return {
         fetchQuotes: () => fetchQuotes()(dispatch),
--- src/actions.js
+++ src/actions.js
@@ -6,7 +6,7 @@ export function fetchQuotes() {
     return (dispatch) => {
         dispatch(startRequest())
 
-        fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+        return fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
         .then((response) => response.json())
         .then(data => {
             dispatch(requestResponse(data))

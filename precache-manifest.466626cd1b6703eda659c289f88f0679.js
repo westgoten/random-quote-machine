4253d339dbf5ac41662e08fbd96ee014ac1c567e
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-09-12 14:49:42 -0300

    Updates

---
+++ precache-manifest.466626cd1b6703eda659c289f88f0679.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bbfa67b897d33d8ab88cc6c0aaaf1d",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "83496ae2717f59017578",
+    "url": "/random-quote-machine/static/css/2.bb1c1f3a.chunk.css"
+  },
+  {
+    "revision": "61416daacad7442bcdaf",
+    "url": "/random-quote-machine/static/css/main.5958cb39.chunk.css"
+  },
+  {
+    "revision": "83496ae2717f59017578",
+    "url": "/random-quote-machine/static/js/2.003bbd99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote-machine/static/js/2.003bbd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61416daacad7442bcdaf",
+    "url": "/random-quote-machine/static/js/main.00ca8e16.chunk.js"
+  },
+  {
+    "revision": "08df3c80334e8eae14de",
+    "url": "/random-quote-machine/static/js/runtime-main.98505e8c.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/random-quote-machine/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/random-quote-machine/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/random-quote-machine/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/random-quote-machine/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/random-quote-machine/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/random-quote-machine/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/random-quote-machine/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/random-quote-machine/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/random-quote-machine/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/random-quote-machine/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/random-quote-machine/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/random-quote-machine/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/random-quote-machine/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/random-quote-machine/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/random-quote-machine/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9be1f1be999be8aa7d711d78503324d.js"
+  "/random-quote-machine/precache-manifest.466626cd1b6703eda659c289f88f0679.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/random-quote-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
